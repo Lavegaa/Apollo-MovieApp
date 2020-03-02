b1cc269a6ebbefd5e3e4fc6f7b2c0f025710a00e
Lavegaa <cksgh405@naver.com>
2020-03-02 20:54:12 +0900

    1-2 Get styled components from nicolas's commit. Get data from api by useQuery. But when i use useParams, it return string that is requiered type Int. So i fix the part at backend. Link to detail page by id. Use variables when i need arguments in useQuery.

--- src/routes/Detail.tsx
+++ src/routes/Detail.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+  query getMovie($id: String!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      summary
+    }
+  }
+`;
 
 export default function Detail() {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: {
+      id
+    }
+  });
+  console.log(loading, data);
+
   return <h1>detail</h1>;
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 import { string } from "prop-types";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIES = gql`
   {
@@ -17,12 +19,47 @@ type movie = {
   medium_cover_image: string;
 };
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 export default function Home() {
   const { loading, data } = useQuery(GET_MOVIES);
-  if (loading) {
-    return "loading...";
-  }
-  if (data && data.movies) {
-    return data.movies.map((m: movie) => <h1>{m.id}</h1>);
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo Movie App</Title>
+        <Subtitle></Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m: movie) => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
 }
