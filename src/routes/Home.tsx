f43b1dbce1b48dbd73b7f6ed1cecc4de113f4dd5
Lavegaa <cksgh405@naver.com>
2020-03-02 20:17:49 +0900

    Chapter 1-1 Set ApolloClient and wrap react application in ApolloProvider. Make basic query to get movies and get data using gql.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import { string } from "prop-types";
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+type movie = {
+  id: number;
+  medium_cover_image: string;
+};
 
 export default function Home() {
-  return <div>home</div>;
+  const { loading, data } = useQuery(GET_MOVIES);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m: movie) => <h1>{m.id}</h1>);
+  }
 }
